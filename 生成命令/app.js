317320000a8bea0c307db853d7923853d49ba63f
Andrew <839153198@qq.com>
2020-01-13 16:18:08 +0800

    add node-create-command

---
+++ 生成命令/app.js
@@ -0,0 +1,28 @@
+const xlsx = require('node-xlsx');
+const fs = require('fs');
+const sheets = xlsx.parse('./1.xlsx');
+var str = ``;
+var options = process.argv;
+var time = options[2]
+
+sheets.forEach(function(sheet){
+  for(var rowId in sheet['data']){
+      if (rowId != 0) {
+        var row = sheet['data'][rowId];
+        str += `
+interface ${row[1]}
+ipv6 nd ra prefix ${row[0]}
+ipv6 address ${row[0]}
+ipv6 nd ns retrans-timer ${time}
+quit
+`
+      }
+  }
+});
+
+fs.writeFile("test.txt", str, function(err) {
+  if(err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+});
---
+++ 生成命令/readme.md
@@ -0,0 +1,15 @@
+# 生成命令
+
+1. 安装环境
+
+    [nodejs](https://nodejs.org/zh-cn/)
+
+2. 安装依赖
+
+        npm install node-xlsx
+
+3. 执行脚本 
+
+        node app.js
+
+4. 查看
